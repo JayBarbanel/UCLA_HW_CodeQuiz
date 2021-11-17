5f6f7765e41acaae45463a176faf5354829fa6dc
Jay <jbarbanel531@gmail.com>
2021-11-16 18:57:49 -0800

    added header, start button and timer (not yet working)

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".startbutton");
+
+var isWin = false;
+var timer;
+var timerCount;
+
+
+function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+}
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (isWin && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                winGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+        }
+    }, 1000);
+}
+
+
+startButton.addEventListener("click", startGame);
