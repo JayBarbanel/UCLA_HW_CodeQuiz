f1e397c58ad80cd625629ba0633c569bbd916984
Jay <jbarbanel531@gmail.com>
2021-11-19 19:38:59 -0800

    added code to make the timer go down if answer is wrong, made an alert pop up if answer is incorrect before going to next question

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// These are my variables that direct back to my HTML
+
 var timerEl = document.querySelector("#timerCount");
 var startButton = document.querySelector(".startButton");
 var questionEl = document.querySelector("#question");
@@ -8,11 +10,15 @@ var answer2El = document.querySelector("#answer2");
 var answer3El = document.querySelector("#answer3");
 var answer4El = document.querySelector("#answer4");
 var qContainerEl = document.querySelector("#qContainer");
+var answersEl = document.querySelector("#answers")
 
+// Global variables 
 var timer;
-var timerCount = 60;
+var timerCount = 100;
 var currentQuestion = 0;
 
+
+// Questions, choices and answers 
 var questions = [{
         question: "Question 1: Inside which HTML element do we put the JavaScript?",
         choices: ["<scripting>", "<script>", "<js>", "<javascript>"],
@@ -39,6 +45,7 @@ var questions = [{
 
 ]
 
+// Timer function, timer starts when 'Start Quiz' button is clicked
 function startTimer() {
     // Sets timer
     timer = setInterval(function() {
@@ -52,16 +59,21 @@ function startTimer() {
     }, 1000);
 }
 
+// Function for when button is clicked, question 2 will load on page 
 function loadQuestion(q) {
     questionEl.textContent = questions[q].question;
     answer1El.textContent = questions[q].choices[0];
+    answer1El.setAttribute("data-answer", questions[q].choices[0]);
     answer2El.textContent = questions[q].choices[1];
+    answer2El.setAttribute("data-answer", questions[q].choices[1]);
     answer3El.textContent = questions[q].choices[2];
+    answer3El.setAttribute("data-answer", questions[q].choices[2]);
     answer4El.textContent = questions[q].choices[3];
+    answer4El.setAttribute("data-answer", questions[q].choices[3]);
     console.log("questionload")
 }
 
-
+// Start game function, also hides and removed hides from certain elements from landing page
 function startGame() {
     startTimer()
     promptEl.classList.add("hide");
@@ -73,3 +85,29 @@ function startGame() {
 
 
 startButton.addEventListener("click", startGame);
+answersEl.addEventListener("click", function(event) {
+    console.log(event.target.value)
+    if (event.target.matches("li")) {
+        var answer = event.target.getAttribute("data-answer");
+        console.log(answer);
+        if (answer === questions[currentQuestion].answers) {
+            // loadQuestion(currentQuestion)
+            console.log("correct")
+        } else {
+            timerCount -= 10;
+            console.log("wrong");
+            alert("Wrong answer, time deducted by 10 seconds")
+        }
+        currentQuestion++
+        loadQuestion(currentQuestion)
+    }
+
+})
+
+
+
+// Need to create a function that allowes you to select an answer button
+// Need the answer selected to appear true or false 
+// Then need to go to the next question 
+// Or i need it to show false, redude time from the clock 
+// Then go to next question
